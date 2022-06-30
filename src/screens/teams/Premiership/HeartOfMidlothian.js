f7dbed3d57676dfff93a180e4e8eb157b8c44ae9
Alastair Lawrie <alastair66@hotmail.co.uk>
2022-06-30 17:58:07 +0100

    various changes made to prem team pages

--- src/screens/teams/Premiership/HeartOfMidlothian.js
+++ src/screens/teams/Premiership/HeartOfMidlothian.js
@@ -18,7 +18,7 @@ import {
     SectionContent,
 } from "react-native-rapi-ui";
 
-export default function ({ navigation }) {
+export default function Aberdeen ({ navigation }) {
     return (
         <Layout>
             <TopNav
@@ -34,43 +34,43 @@ export default function ({ navigation }) {
             <ScrollView>
                 <View style={styles.container}>
                     <View style={styles.header}>
-                        <Text style={styles.clubTitle}>Heart of Midlothian</Text>
+                        <Text style={styles.clubTitle}>Heart of Midlothian </Text>
                         <Image style={styles.clubBadgeFormat}
                         resizeMode="contain"
                         source={require("../../../../assets/club-badges/hearts.png")}/>
                     </View>
 
-                    <View style={styles.clubLocation}>
-                        <Text style={styles.location}> City / Area </Text>
-                        <Text style={styles.locationText}> Edinburgh/ Gorgie </Text>
-                    </View>
-
-                    <View style={styles.generalInformation}>
-                        <Text style={styles.generalInfoText}> League -</Text>
-                        <Text style={styles.generalInfoText}> Cinch Premiership</Text>
-                    </View>
-
-                    <View style={styles.generalInformation}>
-                        <Text style={styles.generalInfoText}> Main Rival - </Text>
-                        <Text style={styles.generalInfoText}> Hibernian </Text>
-                    </View>
-
-                    <View style={styles.generalInformation}>
-                        <Text style={styles.generalInfoText}> Stadium Name - </Text>
-                        <Text style={styles.generalInfoText}> Tynecastle Park</Text>
-                    </View>
-
-                    <View style={styles.generalInformation}>
-                        <Text style={styles.generalInfoText}> Capacity -</Text>
-                        <Text style={styles.generalInfoText}> 20,099</Text>
+                    <View style={{flexDirection:'row', justifyContent:'space-evenly',borderWidth:1, padding:10,backgroundColor:"#800000"}}>
+                        <View style={styles.leftView}>
+                            <Text style={styles.informationText}>City / Region -{"\n"}</Text>
+                            <Text style={styles.informationText}>Year Founded -{"\n"}</Text>
+                            <Text style={styles.informationText}>Main Rival -{"\n"}</Text>
+                            <Text style={styles.informationText}>Stadium Name -{"\n"}</Text>
+                            <Text style={styles.informationText}>Capacity -</Text>
+                        </View>
+                        <View style={styles.rightView}>
+                            <Text style={{color:'white'}}>Edinburgh / Gorgie{"\n"}</Text>
+                            <Text style={{color:'white'}}>1874{"\n"}</Text>
+                            <TouchableOpacity
+                                onPress={() => {
+                                  navigation.navigate("Hibernian");
+                                }}
+                              >
+                                <Text style={{color:'white'}}>
+                                  Hibernian{"\n"}
+                                </Text>
+                              </TouchableOpacity>
+                            <Text style={{color:'white'}}>Tynecastle Park{"\n"}</Text>
+                            <Text style={{color:'white'}}>20,099</Text>
+                        </View>
                     </View>
 
                     <View style={styles.stadiumInfo}>
                         <Text style={styles.clubTitle}> Stadium Location: </Text>
                     </View>
 
-                    <View>
-                        <TouchableOpacity onPress={() => Linking.openURL("https://www.google.com/maps/@55.9385312,-3.2329051,16.27z")}>
+                    <View style={{backgroundColor:"#800000"}}>
+                        <TouchableOpacity onPress={() => Linking.openURL("https://www.google.co.uk/maps/place/Tynecastle+Park/@55.9390849,-3.2337323,17z/data=!3m1!4b1!4m5!3m4!1s0x4887c7aa96299593:0x9d4e5c780ee90305!8m2!3d55.9390849!4d-3.2315436")}>
                         <Image style={styles.stadiumPhoto}
                         resizeMode="contain"
                         source={require("../../../../assets/google-map-photos/hearts_gm.jpg")}/>
@@ -82,26 +82,25 @@ export default function ({ navigation }) {
                             text="Tickets"
                             color="#800000"
                             width={100}
-                            onPress={() => Linking.openURL("https://www.heartsfc.co.uk/tickets-and-hospitality/matchday/upcoming-games")}
+                            onPress={() => Linking.openURL("https:///")}
                         />
                         <Button 
                             text="Fixtures"
                             color="#800000"
                             width={100}
-                            onPress={() => Linking.openURL("https://www.heartsfc.co.uk/fixtures/first-team/fixtures-and-results")}
+                            onPress={() => Linking.openURL("https://www.afc.co.uk/matches/fixtures-results/")}
                         />
                         <Button 
                             text="Players"
                             color="#800000"
                             width={100}
-                            onPress={() => Linking.openURL("https://www.heartsfc.co.uk/squads/first-team/playing-squad")}
+                            onPress={() => Linking.openURL("https://www.afc.co.uk/teams/1st-team/")}
                         />
                     </View>
 
-                    <View style={styles.submissionsLink}>
-                        <Text style={styles.clubTitle}>Click to see previous visit submissions!</Text>
+                    <View style={{padding:16, borderBottomWidth:1,}}>
+                        <Text style={{textDecorationLine:'underline', fontSize:20}}>Click to see previous visit submissions!</Text>
                     </View>
-   
                 </View>
             </ScrollView>
         </Layout>
@@ -112,16 +111,17 @@ const styles = StyleSheet.create({
     container: {
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
-        paddingTop: 20,
+        paddingTop: 10,
         backgroundColor: '#fff',
-        //justifyContent: 'space-around',
-        //alignItems: 'center',
+        borderWidth:1,
+        flex:1,
     },
 
     header: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
+        alignSelf: 'center',
     },
 
     clubTitle: {
@@ -132,6 +132,20 @@ const styles = StyleSheet.create({
     clubBadgeFormat: {
         width: 35,
         height: 38,
+    },
+
+    leftView: {
+        alignItems: "left",
+    },
+
+    rightView:{
+        alignItems: 'right',
+        
+    },
+
+    informationText :{
+        textDecorationLine:'underline',
+        color:'white',
     },
 
     clubLocation: {
@@ -147,28 +161,17 @@ const styles = StyleSheet.create({
         textAlignVertical: 'center',
     },
 
-    generalInformation: {
-        flexDirection: 'row',
-        height: 30,
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        justifyContent: 'space-evenly',
-    },
-
-    generalInfoText: {
-        fontSize: 15,
-    },
-
     stadiumInfo: {
-        backgroundColor: '#ECECEC',
+        backgroundColor: 'white',
         flexDirection: 'column',
+        padding:10,
     },
 
     stadiumPhoto: {
         alignSelf: 'center',
         height: 300,
-        //width: 300,
         width: Dimensions.get('window').width,
+        borderWidth: 1,
     },
 
     buttonBackGround: {
@@ -176,15 +179,8 @@ const styles = StyleSheet.create({
         height: 65,
         backgroundColor: '#fff',
         alignItems: 'center',
-        //flexWrap: 'wrap',
         justifyContent: 'space-evenly',
-        paddingTop: 20,
+        paddingTop: 16,
     },
-
-    submissionsLink: {
-        paddingTop:20,
-        
-
-    }
- })
+ });
 
